5eeb7f29f75d88a51f90fa8027d5d2f69cd349d5
Sanket Sawant <sanketsawant2210@gmail.com>
2025-02-17 23:38:40 +0530

    added contact us page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/frontend_assets/assets";
+import Newsletter from "../components/Newsletter";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div>
+      <div className="text-center text-2xl pt-10 border-t">
+        <Title text1={"CONTACT"} text2={"US"} />
+      </div>
 
-export default Contact
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-20">
+        <img
+          src={assets.contact_img}
+          className="w-full md:max-w-[480px]"
+          alt=""
+        />
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className="font-semibold text-xl text-gray-600">Our Store</p>
+          <p className="text-gray-500">
+            Shop-23, Benchmark Complex <br /> Gangapur Road, Nashik,
+            Maharashtra.
+          </p>
+          <p className="text-gray-500">
+            Tel: 111-222-333 <br /> Email: Forever@gmail.com
+          </p>
+          <p className="font-semibold text-xl text-gray-600">
+            Carrers at Forever
+          </p>
+          <p className="text-gray-500">
+            Learn more about our team and job oppenings.
+          </p>
+          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">Explore Job</button>
+        </div>
+      </div>
+
+      <Newsletter />
+    </div>
+  );
+};
+
+export default Contact;
