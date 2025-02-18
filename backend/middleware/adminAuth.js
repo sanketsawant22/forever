b5cbcac27abef9e3319c613cedcd12dcc5ddb486
Sanket Sawant <sanketsawant2210@gmail.com>
2025-02-18 20:08:01 +0530

    Admin Authentication Middleware

---
+++ backend/middleware/adminAuth.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+
+const adminAuth = async (req, res, next) => {
+    try {
+
+        const { token } = req.headers;
+
+        if (!token) {
+            return res.status(401).json({ success: false, message: "Not Authorised Login Again!" });
+        }
+
+        const token_decode = jwt.verify(token, process.env.JWT_SECRET)
+
+        if (token_decode != process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
+            return res.status(401).json({ success: false, message: "Not Authorised Login Again!" });
+        }
+        else {
+            next();
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: "Internal Server Error" });
+
+    }
+}
+
+export default adminAuth;
