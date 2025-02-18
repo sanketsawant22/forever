b8b26b75548ae652246bf9c0f12498fb06f5ac2c
Sanket Sawant <sanketsawant2210@gmail.com>
2025-02-18 19:45:13 +0530

    admin login authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -81,7 +81,22 @@ const registerUser = async (req, res) => {
 
 // ROUTE FOR ADMIN LOGIN
 const adminLogin = async (req, res) => {
-    
+    try {
+        const { email, password } = req.body;
+
+        if (email == process.env.ADMIN_EMAIL && password == process.env.ADMIN_PASSWORD) {
+            const token = jwt.sign(email + password, process.env.JWT_SECRET);
+
+            res.json({success: true, token: token});
+        }
+        else {
+            res.status(401).json({ success: false, message: "Invalid admin credentials" });
+        }
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: "Internal Server Error" });
+    }
 };
 
 export { loginUser, registerUser, adminLogin };
