b4d5e16bdadbbe9133842c307e5ee08b16fe4570
Sanket Sawant <sanketsawant2210@gmail.com>
2025-02-19 23:20:03 +0530

    set up admin pannel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Add from "./pages/Add.jsx";
+import List from "./pages/List.jsx";
+import Orders from "./pages/Orders.jsx";
+import Login from "./components/Login.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+export const currency = "$"
+const App = () => {
+  const [token, settoken] = useState(localStorage.getItem("token") || "");
+
+  useEffect(() => {
+    localStorage.setItem("token", token);
+  }, [token]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="bg-gray-50 min-h-screen">
+      <ToastContainer />
+      {token === "" ? (
+        <Login settoken={settoken} />
+      ) : (
+        <>
+          <Navbar settoken={settoken} />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
 
-export default App
+            <div
+              className="w-[70%] mx-auto my-8 text-gray-600 text-base"
+              style={{ marginLeft: "max(5vw, 25px)" }}
+            >
+              <Routes>
+                <Route path="/add" exact element={<Add token={token} />} />
+                <Route path="/list" element={<List token={token} />} />
+                <Route path="/order" element={<Orders token={token} />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default App;
